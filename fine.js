615d60a0fff108901ec3de350554786a46f04dc2
nigargr229 <NigarEg@code.edu.az>
2019-01-23 16:20:56 +0400

    000

---
+++ fine.js
@@ -0,0 +1,10 @@
+var fishList={20,10,30,21,12,15,17,15,16,36}
+var max=fishList{0}
+for(var i=1; a<fishList.length; i++)
+{
+    var fish=fishList[i]
+    if(max<fish)
+    {
+        max=fish
+    }
+}
